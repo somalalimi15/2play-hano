bfa5f5b94d3e354148b682e64057e88878e84afd
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-18 18:06:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,7 +82,7 @@ message.member.voiceChannel.join();
 /////
 /////
 client.on('ready', () => {
-    client.channels.find(c => c.id === '500448084849852416').join();
+    client.channels.find(c => c.id === '502466849817100309').join();
 });
 /////
 /////
